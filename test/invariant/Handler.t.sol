326a9e72d9dc8dbe16bf9c9eeea1508369508015
Pranav <pranavlakkadi@gmail.com>
2024-03-12 00:08:06 +0530

    commit

---
+++ test/invariant/Handler.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+
+contract InvariantTest is StdInvariant, Test  {
+    
+}
