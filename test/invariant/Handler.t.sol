43314ed94acd060746faf06016f1ec5959a3e7c8
Pranav <pranavlakkadi@gmail.com>
2024-03-15 00:40:15 +0530

    commit

--- test/invariant/Handler.t.sol
+++ test/invariant/Handler.t.sol
@@ -11,10 +11,13 @@ contract Handler is Test {
     MockWETH private weth;
     ERC20Mock private token;
 
-    uint256 expectedDeltaY;
-    uint256 expectedDeltaX;
-    uint256 startingY;
-    uint256 startingX;
+    // ghost variables basically only used for the test contract (handler)
+    int256 expectedDeltaY;
+    int256 expectedDeltaX;
+    int256 startingY;
+    int256 startingX;
+    int256 actualDeltaX;
+    int256 actualDeltaY;
 
     address liquidityProvider = makeAddr("LP");
 
@@ -27,15 +30,16 @@ contract Handler is Test {
     function deposit(uint256 _wethAmount) public {
         _wethAmount = bound(_wethAmount, 0, type(uint96).max);
 
-        startingX = token.balanceOf(address(this));
-        startingY = weth.balanceOf(address(this));
+        startingX = int256(token.balanceOf(address(this)));
+        startingY = int256(weth.balanceOf(address(this)));
 
-        expectedDeltaY = _wethAmount;
-        expectedDeltaX = pool.getPoolTokensToDepositBasedOnWeth(_wethAmount);
+        expectedDeltaY = int256(_wethAmount);
+        expectedDeltaX = int256(pool.getPoolTokensToDepositBasedOnWeth(_wethAmount));
 
+        // deposit function call
         vm.startPrank(liquidityProvider);
         weth.mint(liquidityProvider, _wethAmount);
-        token.mint(liquidityProvider, expectedDeltaX);
+        token.mint(liquidityProvider, uint256(expectedDeltaX));
 
         weth.approve(address(pool), type(uint256).max);
         token.approve(address(pool), type(uint256).max);
@@ -43,11 +47,26 @@ contract Handler is Test {
         pool.deposit(
             _wethAmount,
             0,
-            expectedDeltaX,
+            uint256(expectedDeltaX),
             uint64(block.timestamp)
         );
-
         vm.stopPrank();
+
+        // actual
+        uint256 endingY = weth.balanceOf(address(this));
+        uint256 endingX = token.balanceOf(address(this));
+
+        actualDeltaX = int256(endingX) - int(startingX);
+        actualDeltaY = int256(endingY) - int(startingY);
+    }
+
+    function swapPoolTokenForWethBasedonOutputWETH(uint256 outputWeth) public {
+        outputWeth = bound(outputWeth, 0, type(uint96).max);
+
+        if (outputWeth >= weth.balanceOf(address(pool))){
+            return;
+        }
+
 
     }
 }
