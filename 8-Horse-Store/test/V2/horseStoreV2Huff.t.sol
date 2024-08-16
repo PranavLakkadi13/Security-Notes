b1cc074a2db4d982723ed7846aec41d27fdc1f76
Pranav <pranavlakkadi@gmail.com>
2024-08-16 22:19:55 +0530

    updated code

--- 8-Horse-Store/test/V2/horseStoreV2Huff.t.sol
+++ 8-Horse-Store/test/V2/horseStoreV2Huff.t.sol
@@ -5,7 +5,7 @@ import {Base_TestV2, horseStoreV2} from "./Base_TestV2.t.sol";
 import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
 
 contract HorseStoreHuffV2 is Base_TestV2 {
-    string public constant horseStoreLocation = "horseStoreV2/HorseStore";
+    string public constant horseStoreLocation = "horseStore_V2/horseStorev2";
 
     function setUp() public override {
         horseStore = horseStoreV2(
